eaa4f45061b53410a5c6de0ed8a30711b57c6720
Aiden <truongtd@bsscommerce.com>
2025-05-28 10:03:36 +0700

    Initial Commit

---
+++ todolISapp/todolISappApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolISappApp.swift
+//  todolISapp
+//
+//  Created by Aiden on 28/5/25.
+//
+
+import SwiftUI
+
+@main
+struct todolISappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ todolISappTests/todolISappTests.swift
@@ -0,0 +1,16 @@
+//
+//  todolISappTests.swift
+//  todolISappTests
+//
+//  Created by Aiden on 28/5/25.
+//
+
+import Testing
+
+struct todolISappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
